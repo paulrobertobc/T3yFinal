ed35947b992c52465f4d62d8ec11634755f921a6
acost <acost@Darwin>
2025-11-24 14:59:10 -0500

    Se agrego la clase Persona

--- src/model/Persona.java
+++ src/model/Persona.java
@@ -5,4 +5,61 @@ package model;
  *
  * @author PAUL
  */
+public abstract class Persona {
+    protected String Nombre;
+    protected String Apellido;
+    protected String DNI;
+    protected String Telefono;
+    protected String Email;
 
+    public Persona() {
+    }
+
+    public Persona(String Nombre, String Apellido, String DNI, String Telefono, String Email) {
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.DNI = DNI;
+        this.Telefono = Telefono;
+        this.Email = Email;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+}
